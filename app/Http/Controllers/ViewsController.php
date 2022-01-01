8904d762a7ff4ebb768a746d8c0a8c9e0f4aaeac
dwiprima01 <dwiprimahandayaniputri@gmail.com>
2022-01-01 16:39:39 +0800

    header

--- app/Http/Controllers/ViewsController.php
+++ app/Http/Controllers/ViewsController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ViewsController extends Controller
 {
     public function index()
     {
-        $response = Http::get('https://api.kawalcorona.com/indonesia');
+        $response = Http::withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer 6|BefdT13vDh4xwJueLrmRHBKGk8CrKOsVNRhHi5fr'
+        ])->get('http://127.0.0.1:8081/api/products');
+
         $data = $response->json();
         dd($data);
 
